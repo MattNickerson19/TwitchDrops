18e5b9dd6b7fb2442ed1acb1b9b34907bb6319ba
MattNickerson19 <matt.nickerson96@gmail.com>
2022-02-10 15:59:30 -0330

    Created rough components, no styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { DropContainer } from "./components/DropContainer";
+import { Drop } from "./components/Drop";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [drops, setDrops] = useState([]);
+
+  useEffect(() => {
+    fetch('add api here')
+      .then((response) => response.json())
+      .then(setDrops)
+  }, []);
+
+  const [faqs, setFaqs] = useState([]);
+
+  useEffect(() => {
+    fetch('add faq api here')
+      .then((response) => response.json())
+      .then(setFaqs)
+  }, []);
+  
+  return 
 }
 
 export default App;
---
+++ frontend/src/components/Drop.js
@@ -0,0 +1,11 @@
+
+export function Drop({name, timeRequired}){
+
+    return (
+        <>
+            <h1>Name: {name}</h1>
+            <h1>Watch Time: {timeRequired}</h1>
+        </>
+        
+    )
+}
