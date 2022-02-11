85210b49116040e46acb3829902cdbe3b925fc0b
readinggaol <dougparker.m@gmail.com>
2022-02-11 11:07:40 -0330

    Server stuff + first route!
    Express, nodemon, mongodb, and a gitignore to keep things clean!
    
    First route fetching stuff is working good with postman.
    
    DB name: pw_drops
    Collection name: drops

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -1,3 +1,26 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+
 
 const app = express();
+app.use(bodyParser.json());
+
+
+//Get all drops currently stored in the DB
+app.get("/api/drops", async (req, res) => {
+    try{
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser : true} );
+        const db = client.db("pw_drops");
+
+        const movies = await db.collection("drops").find({}).toArray();
+        res.status(200).json(movies);
+        client.close();
+    }
+    catch(error){
+        res.status(500).json( {message: "Error connecting to db", error});
+    }
+    
+});
+
+app.listen(8000, () => console.log("Listening on port 8000!"));
