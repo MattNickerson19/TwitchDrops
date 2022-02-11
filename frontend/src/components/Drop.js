2d1e8190f7d3ed117dde40f6b0a130a821a1e0c2
MattNickerson19 <matt.nickerson96@gmail.com>
2022-02-11 11:28:21 -0330

    Updated Attributes

--- frontend/src/components/Drop.js
+++ frontend/src/components/Drop.js
@@ -1,10 +1,12 @@
 
-export function Drop({name, timeRequired}){
+export function Drop({name, timeRequired, streamerName, image, itemDefID}){
 
     return (
         <>
             <h1>Name: {name}</h1>
             <h1>Watch Time: {timeRequired}</h1>
+            <h1>Streamer: {streamerName}</h1>
+            <img src={image}/>
         </>
         
     )
