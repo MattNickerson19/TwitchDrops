b024552bf014e063f5f2c90e104506e51fac654c
readinggaol <dougparker.m@gmail.com>
2022-02-15 20:05:41 -0330

    Header designed and drops loading
    Header component designed, with a new folder in src for images. Drop Container is successfully loading and creating Drop objects.
    
    Renamed the variables in Drop component to match JSON from Eric's example.
    
    Proxy added to the package.json to make the backend serve the front end.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,46 +3,48 @@ import { useState, useEffect } from 'react';
 import {Routes, Route, Link } from "react-router-dom";
 import { DropContainer } from "./components/DropContainer";
 import { Drop } from "./components/Drop";
+import Header from './components/Header';
+import LandingPage from './components/LandingPage';
 
 function App() {
   const [drops, setDrops] = useState([]);
 
-  useEffect(() => {
-    fetch('add api here')
-      .then((response) => response.json())
-      .then(setDrops)
+  useEffect(async () => {
+    const promise = await fetch('/api/drops');
+    const data = await promise.json();
+    setDrops(data);
   }, []);
 
-  const [faqs, setFaqs] = useState([]);
+  // const [faqs, setFaqs] = useState([]);
 
-  useEffect(() => {
-    fetch('add faq api here')
-      .then((response) => response.json())
-      .then(setFaqs)
-  }, []);
+  // useEffect(() => {
+  //   fetch('add faq api here')
+  //     .then((response) => response.json())
+  //     .then(setFaqs)
+  // }, []);
   
   return (
 
     <>
       <div className='App'>
-        /*
+        {/*
           Only using nav tags for place holder. Can implement a proper Nav Bar when design is finalized.
           Could also include our template here as well since it will carry across all of the pages?
-        */
-        <nav>
+        */}
+        {/* <nav>
             <Link to="/">Home</Link>
             <Link to="/FAQS">FAQS</Link>
             <Link to="/PreviousDrops">Previous Drops</Link>
             <Link to="/FutureDrops">Future Drops</Link>
             <Link to="/ProjectWinter">Project Winter</Link>
-        </nav>
+        </nav> */}
         <Routes>
           <Route path="/" element={<LandingPage drops={drops} />} />
-          <Route path="/FAQS" element={<FAQS faqs={faqs}/>} />
+          {/* <Route path="/FAQS" element={<FAQS faqs={faqs}/>} />
           <Route path="/PreviousDrops" element={<PreviousDrops drops={drops} />} />
           <Route path="/FutreDrops" element={<FutureDrops drops={drops} />} />
           <Route path="/ConnectAccounts" element={<ConnectAccounts />} />
-          <Route path="/ProjectWinter" element={<ProjectWinter />} />
+          <Route path="/ProjectWinter" element={<ProjectWinter />} /> */}
         </Routes>
       </div>
     </>  
