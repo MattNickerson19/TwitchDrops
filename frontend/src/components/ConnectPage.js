077308622f3cf9852026213579fff06b3c7d096a
Lawrence-Downey <cooperandalex@outlook.com>
2022-02-16 00:59:20 -0330

    Create ConnectPage.js

---
+++ frontend/src/components/ConnectPage.js
@@ -0,0 +1,58 @@
+import Header from "./Header"
+
+
+
+
+export function ConnectAccounts() {
+
+    const [values, setValues] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleUsernameChange = (event) => {
+        setValues({...values, username: event.target.value})
+    }
+
+    const handlePasswordChange = (event) => {
+        setValues({...values, password: event.target.value})
+    }
+
+
+    return (
+        <>
+        <Header></Header>
+        <div className="form-container">
+            <form class="connect-form">
+                <h2>Please connect your Twitch Account</h2>
+                <input onChange={handleUsernameChange}
+                    value={values.username}
+                    type="text" className="form-field"
+                    placeholder="Twitch Username"
+                    name="username" />
+                <input onChange={handlePasswordChange}
+                    value={values.username}
+                    type="text" className="form-field"
+                    placeholder="Twitch Username"
+                    name="username" />
+            </form>
+            <br /><br /><br /><br />
+            <form class="connect-form">
+                <h2>Please connect your Steam Account</h2>
+                <input onChange={handlePasswordChange}
+                    value={values.username}
+                    type="text" className="form-field"
+                    placeholder="Steam Username"
+                    name="username" />
+                <input onChange={handlePasswordChange}
+                    value={values.username}
+                    type="text" className="form-field"
+                    placeholder="Twitch Username"
+                    name="username" />
+            </form>
+            <button type="submit" className="form-field">Connect</button>
+        </div>
+        </>
+
+    )
+}
