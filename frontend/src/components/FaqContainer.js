7cd035b6ac2263b579359ddc76a0ed614893683f
MattNickerson19 <matt.nickerson96@gmail.com>
2022-02-15 21:24:48 -0330

    Faq dropdown display

--- frontend/src/components/FaqContainer.js
+++ frontend/src/components/FaqContainer.js
@@ -1,11 +1,14 @@
 import { FAQ } from "./FAQ";
 
+
 export function FaqContainer({faqs = []}){
     return (
         <>
+            
             {faqs.map( (faq, i)=> {
                 return (<FAQ key={i} {...faq} /> ) 
             })}
+            
         </>
     );
 }
