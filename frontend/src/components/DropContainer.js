3a7cc520f096182cb6aaccf46c11243d9e8fe20e
readinggaol <dougparker.m@gmail.com>
2022-02-18 09:52:46 -0330

    Parsing the new db data
    Handling new data

--- frontend/src/components/DropContainer.js
+++ frontend/src/components/DropContainer.js
@@ -3,11 +3,13 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 
 export function DropContainer({drops = []}){
+    //filter out only current drops
+    let currentDrops = drops.filter(drop => drop.drop_status === "current");
     return (  
             <Container fluid='auto'>
                 
                     <Row xs={1} md={2} lg={3} xl={4}>
-                    {drops.map( (drop)=> {
+                    {currentDrops.map( (drop) => {
                         return (<Col key={drop.item_name}><Drop key={drop.item_name} {...drop} /></Col> ) 
                     })}
                     </Row>
